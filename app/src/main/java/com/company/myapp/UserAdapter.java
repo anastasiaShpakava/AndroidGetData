c92530a4b4d3d8e792e00a54d4ed60a9bbb92c77
Anastasia Shpakova <shpakavaa@gmail.com>
2020-09-02 23:29:43 +0300

    in progress: added List<Address> addresses

--- app/src/main/java/com/company/myapp/UserAdapter.java
+++ app/src/main/java/com/company/myapp/UserAdapter.java
@@ -14,10 +14,12 @@ import java.util.List;
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     private LayoutInflater layoutInflater;
     private List<User> users;
+    private List<Address> addresses;
     private Context context;
 
-    public UserAdapter(Context context, List<User> users) {
+    public UserAdapter(Context context, List<User> users, List<Address> addresses) {
         this.users = users;
+        this.addresses = addresses;
         this.layoutInflater = LayoutInflater.from(context);
         this.context = context;
     }
@@ -32,11 +34,18 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(UserAdapter.ViewHolder holder, final int position) {
         final User user = users.get(position);
+        final Address address = addresses.get (position);
+        holder.id.setText(String.valueOf(user.getId()));
         holder.name.setText(user.getName());
         holder.username.setText(user.getUsername());
         holder.email.setText(user.getEmail());
         holder.phone.setText(user.getPhone());
         holder.website.setText(user.getWebsite());
+        holder.street.setText(address.getStreet());
+        holder.suite.setText(address.getSuite());
+        holder.city.setText(address.getCity());
+        holder.zipcode.setText(address.getZipcode());
+
 
     }
 
@@ -46,7 +55,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        final TextView name, username, email, phone, website;
+        final TextView name, username, email, phone, website,id, street, suite, city,zipcode ;
 
 
         ViewHolder(View view) {
@@ -56,6 +65,11 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
             email = view.findViewById(R.id.email);
             phone = view.findViewById(R.id.phone);
             website = view.findViewById(R.id.website);
+            id = view.findViewById(R.id.id);
+            street = view.findViewById(R.id.street);
+            suite = view.findViewById(R.id.suite);
+            city = view.findViewById(R.id.city);
+            zipcode = view.findViewById(R.id.zipcode);
         }
     }
 }
